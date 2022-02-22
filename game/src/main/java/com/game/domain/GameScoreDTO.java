6e8fb2e450a8c6c622936ea9daa0120f8efbb40d
DESKTOP-9R5B519\user <leejy12341266@gmail.com>
2022-02-22 17:52:48 +0900

    랭킹페이지처리

--- game/src/main/java/com/game/domain/GameScoreDTO.java
+++ game/src/main/java/com/game/domain/GameScoreDTO.java
@@ -1,11 +1,14 @@
 package com.game.domain;
 
+import com.game.paging.Criteria;
+import com.game.paging.PaginationInfo;
+
 import lombok.Data;
 
 @Data
-public class GameScoreDTO {
+public class GameScoreDTO extends Criteria {
     
     private int rno, rankScore;
-    private String  memId,gameName;
-    
+    private String  memId,gameName,memNick;
+    private PaginationInfo paginationInfo;
 }
