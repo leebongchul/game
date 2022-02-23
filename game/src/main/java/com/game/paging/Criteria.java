1a6d98afbf1e1ec7133475faf30742bd8b66d0d0
DESKTOP-9R5B519\user <leejy12341266@gmail.com>
2022-02-23 18:21:39 +0900

    no message

--- game/src/main/java/com/game/paging/Criteria.java
+++ game/src/main/java/com/game/paging/Criteria.java
@@ -31,7 +31,7 @@ public class Criteria {
 	}
 	
 	public int getStartPage() {
-        return (currentPageNo - 1) * recordsPerPage;
+        return (currentPageNo - 1) * recordsPerPage + 1;
     }
 	
 	public int getEndPage() {
