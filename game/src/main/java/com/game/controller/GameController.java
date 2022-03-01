c7a9c52efea9e135aa498420d71eb28600adb07a
DESKTOP-UASGAEI\khb06 <khb0612@gmail.com>
2022-03-01 19:15:49 +0900

    2048 view단 - controller단 연결 완료. 근데... 좀 수정좀 해야댐 ㅋㅋ;

--- game/src/main/java/com/game/controller/GameController.java
+++ game/src/main/java/com/game/controller/GameController.java
@@ -75,6 +75,44 @@ public class GameController extends UiUtils {
 		return "game/dino";
 
 	}
+	
+	@GetMapping(value = "/2048")
+    public String open2048(@SessionAttribute(name = "loginMem", required = false) MemberDTO loginMember,
+            HttpServletRequest request, Model model) {
+        model.addAttribute("headersession", loginMember);
+        GameScoreDTO user = new GameScoreDTO();
+        user.setGameName("2048");
+        List<GameScoreDTO> top5 = Collections.emptyList();
+
+        if (loginMember == null) {
+            System.out.println("게스트접속");
+            model.addAttribute("member", loginMember);
+            top5 = gameService.selectTop5List(user);
+            model.addAttribute("top5", top5);
+            return "game/2048";
+        }
+
+        System.out.println("유저접속");
+        user.setMemId(loginMember.getMemId());
+        top5 = gameService.selectTop5List(user);
+        GameScoreDTO result = gameService.selectGameScore(user); // 유저정보로 rank_table값 불러옴
+
+        if (result == null) { // DB에 게임점수 테이블에 정보가 없으면 실행
+            int insertresult = gameService.insertGameScore(user); // 유저정보로 rank_table에 게임정보 생성
+            result = gameService.selectGameScore(user); // 생성한 정보를 result에 저장
+            if (insertresult == 0) {
+                System.out.println("insert 실패 ");
+            }
+        }
+
+        // 세션이 유지되면 로그인 홈으로 이동
+        model.addAttribute("member", loginMember);
+        model.addAttribute("game", result);
+        model.addAttribute("top5", top5);
+
+        return "game/2048";
+
+    }
 
 	@GetMapping(value = "/ddong")
 	public String ddongopen(@SessionAttribute(name = "loginMem", required = false) MemberDTO loginMember,
@@ -164,6 +202,18 @@ public class GameController extends UiUtils {
 		user.setGameName("똥피하기");
 		return gameService.updateGameScore(user);
 	}
+	
+	@PostMapping(value = "/game/2048")
+    @ResponseBody
+    public int scoresubmit2048(@SessionAttribute(name = "loginMem", required = false) MemberDTO loginMember,
+            @RequestParam int score) {
+        GameScoreDTO user = new GameScoreDTO();
+        user.setMemId(loginMember.getMemId());
+        user.setRankScore(score);
+        user.setGameName("2048");
+        return gameService.updateGameScore(user);
+    }
+
 
 	@PostMapping(value = "/game/dino")
 	@ResponseBody
