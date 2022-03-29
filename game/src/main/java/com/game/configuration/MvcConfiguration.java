fd12fcf3124f9fa2c94d9c35c0722f8ffb7f8604
DESKTOP-UASGAEI\khb06 <khb0612@gmail.com>
2022-03-29 23:12:48 +0900

    주석 수정

--- game/src/main/java/com/game/configuration/MvcConfiguration.java
+++ game/src/main/java/com/game/configuration/MvcConfiguration.java
@@ -8,29 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.game.interceptor.BlockInterceptor;
 
 @Configuration
-//신규코드
-//@EnableWebMvc
-//@ComponentScan
-//@Slf4j
 public class MvcConfiguration implements WebMvcConfigurer {
 
 	@Autowired
 	BlockInterceptor blockInterceptor;
 
-	// 신규코드
-	/*
-	 * @Bean public BlockInterceptor blockInterceptor() { return new
-	 * BlockInterceptor(); }
-	 */
-//    
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-//        LoginIntercepter loginIntercepter = ;
-//      기존코드
-//        registry.addInterceptor(new BlockInterceptor()) 
-		// 신규코드
 		registry.addInterceptor(blockInterceptor).addPathPatterns("/member/login", "/member/kakaologin")
-//                .excludePathPatterns();
 				.excludePathPatterns("/css/**", "/fonts/**", "/plugin/**", "/scripts/**");
 
 	}
